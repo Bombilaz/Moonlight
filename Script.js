6b1e8fb7fc6b5681d7e8993efbf42cf7c5db785b
Alex <Retho@yandex.ru>
2021-07-28 21:48:28 +0300

    Added main script

---
+++ Script.js
@@ -0,0 +1,10 @@
+let stars = document.getElementById("stars");
+let moonLight = document.getElementById("text");
+let button = document.getElementById("btn");
+let mountainsFront = document.getElementById("mfront");
+let mountainsBack = document.getElementById("mbehind");
+let moon = getElementById("moon");
+window.addEventListener('scroll', function(){
+   let value = window.scrollY;
+   moonLight.style.color = "Red"
+})
